5d86075e4d2e8aa0cbde5b22e00796d524cfc6b6
Bahman Zamani <hamparvaz321@gmail.com>
2025-08-11 12:56:34 +0330

    Add initial age requirement program

---
+++ Age requirement.py
@@ -0,0 +1,12 @@
+# برنامه شرط سني (Age requirement)
+#START
+while True:
+    try :
+        age = input ("Please enter your age:")
+        if int(age) >= 18 :
+            print ("You can register for certification.")
+        else:
+            print ("You can't register for certification.")
+    except ValueError:
+        print ("Error: Input is not a number! Please enter a number.")
+#END
